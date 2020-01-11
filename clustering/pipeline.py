6dc7b2946e69da5f3a527dafcf7a4b6da991520e
VaCH2 <chiel.van.asseldonk@hotmail.com>
2020-01-11 20:24:19 +0100

    add MeanShift, DBSCAN and OPTICS cluster algo

--- clustering/pipeline.py
+++ clustering/pipeline.py
@@ -1,3 +1,5 @@
+#%%
+
 import pandas as pd
 import numpy as np
 from datatrans import Dataset
@@ -7,11 +9,10 @@ from sklearn.cluster import MeanShift
 from sklearn.cluster import SpectralClustering
 from sklearn.cluster import AgglomerativeClustering
 from sklearn.cluster import DBSCAN
-#from sklearn.cluster import OPTICS
+from sklearn.cluster import OPTICS
 from sklearn.metrics import silhouette_score
 from sklearn.metrics import davies_bouldin_score
 from sklearn.metrics import calinski_harabasz_score
-#from sklearn.metrics import homogeneity_score #Deze kan niet omdat je hier een ground truth voor nodig hebt
 
 original_df = Dataset(7, False).getDf
 df = normalize(original_df)
@@ -20,7 +21,11 @@ k_sizes = [2,3,4,5,6,7,8]
 
 algos = {'kmeans' : KMeans, \
     'specclu' : SpectralClustering, 
-    'aggclu' : AgglomerativeClustering} #(), MeanShift(), SpectralClustering(), AgglomerativeClustering(), DBSCAN(), OPTICS()]
+    'aggclu' : AgglomerativeClustering} 
+    
+nonk_algos = {'meanshift' : MeanShift, \
+    'dbscan' : DBSCAN,
+    'optics' : OPTICS} 
 
 metrics = {'silhouette' : silhouette_score, \
     'davies' : davies_bouldin_score, 
@@ -28,6 +33,10 @@ metrics = {'silhouette' : silhouette_score, \
 
 def clustering(df, algo, k):
     model = algo(n_clusters=k)
+    return model.fit_predict(df)
+
+def nonk_clustering(df, algo):
+    model = algo()
     return model.fit_predict(df)
 
 def calculate_cluster_score(df, k_sizes, algos, metrics):
@@ -40,6 +49,18 @@ def calculate_cluster_score(df, k_sizes, algos, metrics):
             for metric_name, metric in metrics.items():
                 score = metric(df, result)
                 scores.loc[(algo_name, metric_name), k] = score
+    return scores
+
+def calculate_nonk_cluster_score(df, nonk_algos, metrics):
+    index = pd.MultiIndex.from_product(iterables=[nonk_algos.keys(), metrics.keys()], names=['algo', 'evaluation'])
+    scores = pd.DataFrame(index=index, columns=range(10))
+
+    for algo_name, algo in nonk_algos.items():
+        result = nonk_clustering(df, algo)
+        max_cluster = max(np.unique(result))
+        for metric_name, metric in metrics.items():
+            score = metric(df, result)
+            scores.loc[(algo_name, metric_name), max_cluster] = score
     return scores
 
 def create_cluster_dfs(original_df, algos, k):
@@ -64,3 +85,11 @@ def get_cluster_stats(df_dict):
 clusters = create_cluster_dfs(original_df, algos, 2)
 results = get_cluster_stats(clusters)
 results.to_excel('cluster_stats.xlsx')
+
+#%%
+original_df
+
+# %%
+nonk_clustering(df, DBSCAN)
+
+# %%
