e835823b90330d5204543ce65adcbeef69fab70b
VaCH2 <chiel.van.asseldonk@hotmail.com>
2020-01-20 21:33:26 +0100

    not necessary anymore

--- data/alldata.py
+++
@@ -1,8 +0,0 @@
-from data import Data
-
-datasets = ['all' ,'industry' , 'example' ,'a4c', 'forge', 'puccini']
-metric_types = ['general', 'tosca', 'tosca_and_general']
-             
-for ds in datasets:
-    for met in metric_types:
-        Data(met, ds)
