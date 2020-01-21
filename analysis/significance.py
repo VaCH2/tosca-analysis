136f2545ec0ec39b687928cdf67210870d306985
VaCH2 <chiel.van.asseldonk@hotmail.com>
2020-01-21 23:45:38 +0100

    fix dataframe append

--- analysis/significance.py
+++ analysis/significance.py
@@ -13,10 +13,9 @@ class Significance():
             raise ValueError('datasets do not contain the same columns')
 
         uncorrected_p_values = self.__calc_sig()
-        uncorrected_p_values = uncorrected_p_values.dropna() 
-        self.sig = self.__multitest_correction(uncorrected_p_values, 0.05)
-        rejected_features = self.sig[['rejected', 'corr_p_values']]
-        self.rejected_features = rejected_features[rejected_features['rejected'] == True]
+        self.uncorrected_p_values = uncorrected_p_values.dropna() 
+        self.sig = self.__multitest_correction(self.uncorrected_p_values, 0.05)
+        self.rejected_features = self.sig[self.sig['rejected'] == True]
 
     
     def __calc_sig(self):
@@ -46,6 +45,6 @@ class Significance():
         Returns a corrected p-value per hypothesis and a rejection boolean based on the provided alpha'''
         values = p_values['p_values'].tolist()
         testcorrection = multipletests(values, alpha, 'fdr_bh')
-        p_values['rejected'] = testcorrection[0]
-        p_values['corr_p_values'] = testcorrection[1]
+        d = {'rejected' : testcorrection[0], 'corr_p_values' : testcorrection[1]}
+        p_values = pd.DataFrame(data=d, index=self.uncorrected_p_values.index)
         return p_values
