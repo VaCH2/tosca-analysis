48ae00323f3e4e11dab7ce7575fe3b88094e985b
VaCH2 <chiel.van.asseldonk@hotmail.com>
2020-01-21 10:48:23 +0100

    add sparsity and constants calculation

--- analysis/preprocessing.py
+++ analysis/preprocessing.py
@@ -6,11 +6,25 @@ import pickle
 class Preprocessing():
     def __init__(self, df, corr=False, pca=False):
         self.df = df
+        self.sparsity = self.calc_sparsity()
+        self.constants = self.constantvalues()
         if corr == True:
             self.df = self.correlation()
         if pca == True:
             self.df = self.pca()
 
+    def calc_sparsity(self):
+        '''Calculate the sparsity of the selected data'''
+        zeroes = 0
+        for column in self.df.columns:
+            zeroes += np.count_nonzero(self.df[column] == 0)
+        return zeroes / (self.df.shape[0] * self.df.shape[1])
+
+    def constantvalues(self):
+        '''Drop the variables which have a contant value'''
+        constant_columns = [column for column in self.df.columns if len(self.df[column].unique()) < 2]
+        self.df = self.df.drop(self.df[constant_columns], axis=1)
+        return constant_columns
     
     def correlation(self):
         '''Drop the high correlation variables, and store the high correlation values.'''
