6c957a28025e30f013aec67b82c01e9560e67a8d
VaCH2 <chiel.van.asseldonk@hotmail.com>
2020-01-27 22:47:41 +0100

    .

--- analysis/pipeline2.py
+++ analysis/pipeline2.py
@@ -30,36 +30,3 @@ datasets2 = [Stats(data)]
 results2 = get_stats(datasets2)
 
 #Stats laten duidelijk zien dat ook de anomaly detection focuessed op de grote scripts.
-
-#%%
-
-def check_repo(df):
-    count = {'a4c' : 0, 'forge' : 0, 'puccini' : 0}
-    a4c = df.index.str.contains('A4C')
-    count['a4c'] = np.sum(a4c)
-    forge = df.index.str.contains('Forge')
-    count['forge'] = np.sum(forge)
-    puc = df.index.str.contains('Puccini')
-    count['puccini'] = np.sum(puc)
- 
-    return count
-
-def check_purpose(df):
-    count = {'industry' : 0, 'example' : 0}
-    industry = df.index.str.contains('Total Industry')
-    count['industry'] = np.sum(industry)
-    example = df.index.str.contains('Total Examples')
-    count['example'] = np.sum(example)
-
-    return count
-
-check_repo(anomalies.outliers)
-purp_anomalies = AnomalyDetector(data, cutoff=2).outliers
-check_purpose(purp_anomalies)
-
-purp_anomalies['ttb_check'].value_counts()
-#In alle 3 de gevallen zie je dat er geen onderscheid wordt gemaakt op de splits die wij voor ogen hadden. 
-#Anomalies zijn gelijk verdeeld. Dus dit laat zien dat er geen verschil is op basis van anomalies. 
-
-
-
