28700cb1f5909c2721f4262e6648c5e5d1fab1fe
VaCH2 <chiel.van.asseldonk@hotmail.com>
2020-01-27 11:06:43 +0100

    update anomaly class

--- analysis/anomaly.py
+++ analysis/anomaly.py
@@ -12,11 +12,16 @@ from pyod.models.lof import LOF
 from pyod.models.mcd import MCD
 from pyod.models.ocsvm import OCSVM
 
+from pyod.utils.utility import standardizer
+from pyod.models.combination import maximization
+import matplotlib.pyplot as plt
+
 class AnomalyDetector():
 
-    def __init__(self, data):
+    def __init__(self, data, cutoff=3):
         self.df = data.df
-        self.scores = self.__train_classifiers()
+        self.df['outlier_score'] = self.__train_classifiers()
+        self.outliers = self.__identify_outliers(cutoff)
 
     def __load_classifiers(self):
         outliers_fraction = 0.05
@@ -47,23 +52,39 @@ class AnomalyDetector():
 
         return classifiers
 
-    #Ze failen hier allemaal? Wat gaat er mis?
     def __train_classifiers(self):
         scaler = MinMaxScaler(feature_range=(0,1))
         X = scaler.fit_transform(self.df.copy())
-        print(X)
+        print(type(X))
         classifiers = self.__load_classifiers()
         scores = np.zeros([X.shape[0], len(classifiers)])
         for i, (clf_name, clf) in enumerate(classifiers.items()):
             try:
                 clf.fit(X)
-                scores[:, clf_name] = clf.decision_scores_
+                scores[:, i] = clf.decision_scores_
             except Exception as e:
                 print("Failed for ", clf_name)
                 print("because of ", e)
-        return scores
+
+            
+        standard_scores = standardizer(scores)
+        combined_scores = maximization(standard_scores)
+        return combined_scores
+
+    def __identify_outliers(self, cutoff):
+        outliers = self.df[self.df['outlier_score'] > cutoff]
+        return outliers
+
+
 
 data = Data('tosca_and_general', 'all')
-anomaly = AnomalyDetector(data)
+anomaly = AnomalyDetector(data, 2)
 
-    
+# from pyod.utils.data import generate_data, get_outliers_inliers
+
+# #generate random data with two features
+# X_train, Y_train = generate_data(n_train=200,train_only=True, n_features=2)  
+
+plt.plot(sorted(anomaly.df['outlier_score']))
+plt.show()
+
