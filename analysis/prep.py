7a85270e524fefab6b0f4042b49674908335a39a
VaCH2 <chiel.van.asseldonk@hotmail.com>
2020-01-28 01:56:19 +0100

    create option for braycurtis distance function

--- analysis/prep.py
+++ analysis/prep.py
@@ -2,11 +2,18 @@ import pandas as pd
 import numpy as np
 from data import Data
 import pickle
+from scipy.stats import chi2_contingency as chi2
+from scipy.spatial.distance import braycurtis
 
 class Preprocessing():
-    def __init__(self, data, constants=None, corr=None, pca=False, anomalies=False):
+    def __init__(self, data, constants=None, corr=None, pca=False, anomalies=False, chi=False):
         '''For constants and corr please provide the list of to be dropped columns'''
-        self.df = data.df
+
+        if isinstance(data, Data):
+            self.df = data.df.head(10)
+        elif isinstance(data, pd.DataFrame):
+            self.df = data.head(10)
+
         self.anomalypercentage = None
         to_drop = []
         if corr != None:
@@ -16,26 +23,31 @@ class Preprocessing():
             to_drop.extend(constants)
         
         to_drop = list(set(to_drop))
-        # print('len dropped columns: ', len(to_drop))
-        # print('dropped columns: ', to_drop)
         
         self.df = self.df.drop(to_drop, axis=1)
         
         if anomalies != None:
             self.anomalypercentage = self.filter_anomalies()
 
+        if chi:
+            self.df = self.__transform_distance()
 
-    
+
+    def __transform_distance(self):
+        matrix = pd.DataFrame(index=self.df.index, columns=self.df.index)
+        # Werkt niet omdat de data veel te sparse is.. Stond ergens een minimale count van 5 per kolom. 
+        #Nu maar braycurtis genomen maar weet niet of hier wat haken en ogen aan zitten..
+        combs = [ ((x,y), braycurtis(self.df.loc[[x], :].to_numpy(),self.df.loc[[y], :].to_numpy())) for x in list(self.df.index) for y in list(self.df.index)]
+        for element in combs:
+            matrix.loc[element[0][0], element[0][1]] = element[1]
+        return matrix
+
+
+
+
     def filter_anomalies(self):
-        pass
+        return 0
 
 
-
-#df = pickle.load(open('../temp_data/{}_{}_raw_df'.format('tosca_and_general', 'all'), 'rb'))
-
-# df = Data('tosca_and_general', 'all').df
-# test = Selection(df)
-
-# for dat in ['all', 'industry', 'example', 'a4c', 'forge', 'puccini']:
-#     for met in ['general', 'tosca', 'tosca_and_general']:
-#         Data(met, dat) 
+data = Data('tosca_and_general', 'all')
+prep = Preprocessing(data, chi=True)
