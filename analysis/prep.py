4079acedb575830760781ebcc6910fe43cd5dd8f
VaCH2 <chiel.van.asseldonk@hotmail.com>
2020-02-05 18:15:01 +0100

    add custom distance functions

--- analysis/prep.py
+++ analysis/prep.py
@@ -3,11 +3,13 @@ import numpy as np
 from data import Data
 import pickle
 from scipy.stats import chi2_contingency as chi2
-from scipy.spatial.distance import braycurtis
+from sklearn.metrics import pairwise_distances
+from scipy.stats import spearmanr
 
 class Preprocessing():
-    def __init__(self, data, constants=None, corr=None, pca=False, anomalies=False, customdistance=False):
-        '''For constants and corr please provide the list of to be dropped columns'''
+    def __init__(self, data, constants=None, corr=None, pca=False, anomalies=False, customdistance=None):
+        '''For constants and corr please provide the list of to be dropped columns.
+        For custom distance provide valid distance (spearman, braycurtis, cosine, l1)'''
 
         if isinstance(data, Data):
             self.df = data.df
@@ -29,23 +31,35 @@ class Preprocessing():
         if anomalies != None:
             self.anomalypercentage = self.filter_anomalies()
 
-        if customdistance:
+        if customdistance in ['spearman', 'braycurtis', 'cosine', 'l1']:
             try:
-                self.df = pickle.load(open('../temp_data/tosca_and_general_all_braycurtisdistance', 'rb'))
+                self.df = pickle.load(open('../temp_data/tosca_and_general_all_{}'.format(customdistance), 'rb'))
                 
             except (OSError, IOError) as e:
-                self.df = self.__transform_distance()
-                pickle.dump(self.df, open('../temp_data/tosca_and_general_all_braycurtisdistance', 'wb'))
+                self.df = self.__transform_distance(customdistance)
+                pickle.dump(self.df, open('../temp_data/tosca_and_general_all_{}'.format(customdistance), 'wb'))
+        else:
+            print('Invalid distance function! Valid funtions are: spearman, braycurtis, cosine, l1')
 
 
-    def __transform_distance(self):
-        matrix = pd.DataFrame(index=self.df.index, columns=self.df.index)
-        # Werkt niet voor chi2 omdat de data veel te sparse is.. Stond ergens een minimale count van 5 per kolom. 
-        #Nu maar braycurtis genomen maar weet niet of hier wat haken en ogen aan zitten..
-        # Hier staat wat uitgelegd over de 2: http://84.89.132.1/~michael/stanford/maeb5.pdf 
-        combs = [ ((x,y), braycurtis(self.df.loc[[x], :].to_numpy(),self.df.loc[[y], :].to_numpy())) for x in list(self.df.index) for y in list(self.df.index)]
-        for element in combs:
-            matrix.loc[element[0][0], element[0][1]] = element[1]
+    def __transform_distance(self, customdistance):
+        data = self.df.to_numpy()
+
+        if customdistance == 'spearman':
+            distances, pval = spearmanr(data)
+            matrix = pd.DataFrame(data=distances, index=self.df.index, columns=self.df.index)
+
+        else:
+            distances = pairwise_distances(data, metric=customdistance)
+            matrix = pd.DataFrame(data=distances, index=self.df.index, columns=self.df.index)
+
+        # matrix = pd.DataFrame(index=self.df.index, columns=self.df.index)
+        # # Werkt niet voor chi2 omdat de data veel te sparse is.. Stond ergens een minimale count van 5 per kolom. 
+        # #Nu maar braycurtis genomen maar weet niet of hier wat haken en ogen aan zitten..
+        # # Hier staat wat uitgelegd over de 2: http://84.89.132.1/~michael/stanford/maeb5.pdf 
+        # combs = [ ((x,y), braycurtis(self.df.loc[[x], :].to_numpy(),self.df.loc[[y], :].to_numpy())) for x in list(self.df.index) for y in list(self.df.index)]
+        # for element in combs:
+        #     matrix.loc[element[0][0], element[0][1]] = element[1]
         return matrix
 
 
