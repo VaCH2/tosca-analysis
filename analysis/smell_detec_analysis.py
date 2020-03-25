0c7cb04fabb145a9b0199004d561d93c400003ed
VaCH2 <chiel.van.asseldonk@hotmail.com>
2020-03-25 23:13:43 +0100

    test significance 1 smell

--- analysis/smell_detec_analysis.py
+++ analysis/smell_detec_analysis.py
@@ -2,10 +2,44 @@
 #Met de main kun je de rules opnieuw uitrekenen, maar is eigenlijk niet meer nodig tenzij er wat wordt aangepast. 
 
 import sys
-sys.path.append('C:/Users/s145559/OneDrive - TU Eindhoven/School/JADS/Jaar 2/Thesis/RADON PROJECT/GIT projects/TOSCASmellDetector')
-from smelldetector.longstatement import evaluate_script_with_rule
-from smellmetrics.cpl import CPL
-from smelldetector.rule_calculator import main
+#sys.path.append('C:/Users/s145559/OneDrive - TU Eindhoven/School/JADS/Jaar 2/Thesis/RADON PROJECT/GIT projects/TOSCASmellDetector')
+sys.path.append(r'C:\Users\s145559\OneDrive - TU Eindhoven\School\JADS\Jaar 2\Thesis\RADON PROJECT\GIT projects\TOSCASmellDetector')
+from smells.detector import longstatement
+from smells.detector import toomanyattributes
 
-path = r'C:\Users\s145559\OneDrive - TU Eindhoven\School\JADS\Jaar 2\Thesis\RADON PROJECT\GIT projects\tosca-metrics\test_files\cpl.yaml'
-evaluate_script_with_rule(path)
+from scipy.stats import mannwhitneyu
+from data import Data
+
+#nog fixen dat alle opties makkelijk gedaan kunnen worden
+#Ff zorgen dat tussenresulaten opgeslagen worden
+
+def main(set1, set2):
+    sets = [set1, set2]
+    file_ixs = get_data(sets)
+    smells = get_smells(file_ixs)
+    sig = get_sig(smells, set1, set2)
+    return sig
+
+def get_data(datasets):
+    ix_lists = {}
+    for dataset in datasets:
+        data = Data('tosca_and_general', dataset)
+        ix_list = [r'{}'.format(ix) for ix in data.df.index]
+        ix_lists[dataset] = ix_list
+    return ix_lists
+
+
+def get_smells(ix_lists):
+    smells = {}
+    for dataset, ix_list in ix_lists.items():
+        tma = [toomanyattributes.evaluate_script_with_rule(ix) for ix in ix_list]
+        #ls = ...
+        smells[dataset] = tma
+    return smells
+
+
+def get_sig(smell_data, set1, set2):
+    stat, p = mannwhitneyu(smell_data[set1], smell_data[set2])
+    return (stat, p)
+
+main('industry', 'example')
