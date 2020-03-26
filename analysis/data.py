6f72b87dcaf46a713e6350e47cfdcd656ebb3962
VaCH2 <chiel.van.asseldonk@hotmail.com>
2020-03-26 20:46:54 +0100

    data class organized by split

--- analysis/data.py
+++ analysis/data.py
@@ -4,45 +4,116 @@ from toscametrics import calculator
 import pickle
 
 class Data():
-    def __init__(self, metrics_type, dataset, file_type='all'):
-        datasets_dir = {
-            'all'       :   r'C:\Users\s145559\OneDrive - TU Eindhoven\School\JADS\Jaar 2\Thesis\RADON PROJECT\Data\All\All',
-            'industry'  :   r'C:\Users\s145559\OneDrive - TU Eindhoven\School\JADS\Jaar 2\Thesis\RADON PROJECT\Data\All\Total Industry',
-            'example'   :   r'C:\Users\s145559\OneDrive - TU Eindhoven\School\JADS\Jaar 2\Thesis\RADON PROJECT\Data\All\Total Examples',
-            'repos'     :   r'C:\Users\s145559\OneDrive - TU Eindhoven\School\JADS\Jaar 2\Thesis\RADON PROJECT\Data\Repositories',
-            'a4c'       :   r'C:\Users\s145559\OneDrive - TU Eindhoven\School\JADS\Jaar 2\Thesis\RADON PROJECT\Data\Repositories\A4C',
-            'forge'     :   r'C:\Users\s145559\OneDrive - TU Eindhoven\School\JADS\Jaar 2\Thesis\RADON PROJECT\Data\Repositories\Forge',
-            'puccini'   :   r'C:\Users\s145559\OneDrive - TU Eindhoven\School\JADS\Jaar 2\Thesis\RADON PROJECT\Data\Repositories\Puccini'
-        }
-
-        file_types = ['all', 'topology', 'custom', 'both', 'none']
-
-        if not dataset in datasets_dir.keys():
-            raise ValueError('Enter a valid dataset (all, industry, example, repos, a4c, forge, puccini)')
-
-        if not file_type in file_types:
-            raise ValueError('Enter a valid file type (all, topology, custom, both, none)')
+    #desnoods die metrics type hierin code ipv in die calculator en dan gewoon die hardcoden op 'all'
+    def __init__(self, split='all', metrics_type='tosca_and_general'):
 
         try:
-            self.raw_df = pickle.load(open('../temp_data/{}_{}_raw_df'.format(metrics_type, dataset), 'rb'))
+            raw_df = pickle.load(open('../temp_data/all_raw_df', 'rb'))
             
         except (OSError, IOError) as e:
-            files = self.get_yaml_files(datasets_dir[dataset])
-            json_data = self.json_data(metrics_type, files)
-            self.raw_df = self.to_df(json_data)
-            pickle.dump(self.raw_df, open('../temp_data/{}_{}_raw_df'.format(metrics_type, dataset), 'wb'))
+            files = self.get_indices('all')
+            json_data = self.json_data(metrics_type, files.get('all'))
+            raw_df = self.to_df(json_data)
+            pickle.dump(raw_df, open('../temp_data/all_raw_df', 'wb'))
 
-        try:
-            self.df = pickle.load(open('../temp_data/{}_{}_df'.format(metrics_type, dataset), 'rb'))
+        raw_size = raw_df.shape[0]
+
+        df = self.cleaning(raw_df)
+        #df = self.valid_tosca_file(df)
+
+        cleaned_size = df.shape[0]
+        self.droppedrows = raw_size - cleaned_size
+
+        split_indices = self.get_indices(split, df)
+
+        self.dfs = {split_element : df.loc[indices] for split_element, indices in split_indices.items()}
         
-        except (OSError, IOError) as e:
-            self.df = self.cleaning(self.raw_df)
-            pickle.dump(self.df, open('../temp_data/{}_{}_df'.format(metrics_type, dataset), 'wb'))
 
-        self.typefilterpercentage = None
+    def get_indices(self, split, df):
+        data_path = r'C:\Users\s145559\OneDrive - TU Eindhoven\School\JADS\Jaar 2\Thesis\RADON PROJECT\Data\Repositories'
+        repos = os.listdir(data_path)
+        professionalities = ['Example', 'Industry']
 
-        if file_type != 'all':
-            self.typefilterpercentage = self.filter_filetype(file_type)
+        if split == 'repo':
+            split_paths ={repo : [data_path + r'\{}'.format(repo)] for repo in repos}
+
+        elif split == 'professionality':
+            repo_paths ={repo : data_path + r'\{}'.format(repo) for repo in repos}
+            split_paths = {}
+            
+            for prof in professionalities:
+                split_paths[prof] = [repo_path + r'\{}'.format(prof) for repo_path in repo_paths.values()]
+
+
+        elif split == 'purpose':
+            split_files = self.filter_filetype(df)
+
+        elif split == 'all':
+            split_paths = {'all' : [data_path]}
+
+        else:
+            raise ValueError
+
+        if split != 'purpose':
+            split_files = {}
+            for split, paths in split_paths.items():
+                
+                files = []
+                for path in paths:
+                    files.extend(self.get_yaml_files(path))
+                
+                split_files[split] = files
+        
+        return split_files
+
+
+
+
+
+
+
+
+
+
+
+        # datasets_dir = {
+        #     'all'       :   r'C:\Users\s145559\OneDrive - TU Eindhoven\School\JADS\Jaar 2\Thesis\RADON PROJECT\Data\All\All',
+        #     'industry'  :   r'C:\Users\s145559\OneDrive - TU Eindhoven\School\JADS\Jaar 2\Thesis\RADON PROJECT\Data\All\Total Industry',
+        #     'example'   :   r'C:\Users\s145559\OneDrive - TU Eindhoven\School\JADS\Jaar 2\Thesis\RADON PROJECT\Data\All\Total Examples',
+        #     'repos'     :   r'C:\Users\s145559\OneDrive - TU Eindhoven\School\JADS\Jaar 2\Thesis\RADON PROJECT\Data\Repositories',
+        #     'a4c'       :   r'C:\Users\s145559\OneDrive - TU Eindhoven\School\JADS\Jaar 2\Thesis\RADON PROJECT\Data\Repositories\A4C',
+        #     'forge'     :   r'C:\Users\s145559\OneDrive - TU Eindhoven\School\JADS\Jaar 2\Thesis\RADON PROJECT\Data\Repositories\Forge',
+        #     'puccini'   :   r'C:\Users\s145559\OneDrive - TU Eindhoven\School\JADS\Jaar 2\Thesis\RADON PROJECT\Data\Repositories\Puccini'
+        # }
+
+        # purpose_types = ['all', 'topology', 'custom', 'both', 'none']
+
+        # if not dataset in datasets_dir.keys():
+        #     raise ValueError('Enter a valid dataset (all, industry, example, repos, a4c, forge, puccini)')
+
+        # if not file_type in file_types:
+        #     raise ValueError('Enter a valid file type (all, topology, custom, both, none)')
+
+        # try:
+        #     self.raw_df = pickle.load(open('../temp_data/{}_{}_raw_df'.format(metrics_type, dataset), 'rb'))
+            
+        # except (OSError, IOError) as e:
+        #     files = self.get_yaml_files(datasets_dir[dataset])
+        #     json_data = self.json_data(metrics_type, files)
+        #     self.raw_df = self.to_df(json_data)
+        #     pickle.dump(self.raw_df, open('../temp_data/{}_{}_raw_df'.format(metrics_type, dataset), 'wb'))
+
+        # try:
+        #     self.df = pickle.load(open('../temp_data/{}_{}_df'.format(metrics_type, dataset), 'rb'))
+        
+        # except (OSError, IOError) as e:
+        #     self.df = self.cleaning(self.raw_df)
+        #     pickle.dump(self.df, open('../temp_data/{}_{}_df'.format(metrics_type, dataset), 'wb'))
+
+        # self.typefilterpercentage = None
+
+        # if file_type != 'all':
+        #     self.typefilterpercentage = self.filter_filetype(file_type)
 
     def get_yaml_files(self, path):
         extensions = ['.yaml', '.yml']
@@ -76,45 +147,51 @@ class Data():
         df = pd.DataFrame.from_dict(flat_dict, orient='index')
         return df
 
+
+
+    def valid_tosca_file(self, df):
+        #hier alles eruit flikkeren wat vreemd is
+        #tosca_definition
+        #Similarity checken (weet nog niet hoe, want heb alleen de metrics)
+
+        pass
+
+
+
     def cleaning(self, df):       
         #Drop NaN rows and error columns, and make numeric
         df = df.drop(labels=(df.filter(regex='msg').columns), axis=1)
+        start = df.shape[0]
         df = df.dropna()
         cols = df.select_dtypes(include=['bool', 'object']).columns
         df[cols] = df[cols].astype(int)
         df = df.dropna()
+        self.nan_dropped = start - df.shape[0]
+        print('Rows with NaN dropped: ', nan_dropped)
         return df
 
     
-    def filter_filetype(self, filetype):
+    def filter_filetype(self, original_df):
         '''Filter on the file type. A file could be a service template, or containing
-        custom type definitions, both or none of these two. It assigns the filtered df
-        to self.df and assings the filtered percentage to typefilterpercentage'''
+        custom type definitions, both or none of these two. It returns a dictionary with the indices
+        belonging to each purpose.'''
 
-        if not filetype in ['topology', 'custom', 'both', 'none']:
-            raise ValueError('Enter a valid file type (topology, custom, both, none)')
+        df = original_df.copy()
 
-        cus_df = self.df[['cdnt_count', 'cdrt_count', 'cdat_count', 
-        'cdct_count', 'cddt_count', 'cdgt_count', 'cdit_count', 'cdpt_count']]
+        custom_indicators = ['cdnt_count', 'cdrt_count', 'cdat_count', 'cdct_count', 'cddt_count', 'cdgt_count', 'cdit_count', 'cdpt_count']
 
+        cus_df = df[custom_indicators]
         non_df = cus_df[(cus_df == 0).all(1)] 
-        self.df['custom_def'] = [False if x in non_df.index else True for x in self.df.index]
 
-        if filetype == 'topology':
-            result = self.df[(self.df['ttb_check'] == 1) & (self.df['custom_def'] == False)]
+        df['custom_def'] = [False if x in non_df.index else True for x in df.index]
 
-        if filetype == 'custom':
-            result = self.df[(self.df['ttb_check'] == 0) & (self.df['custom_def'] == True)]
-
-        if filetype == 'both':
-            result = self.df[(self.df['ttb_check'] == 1) & (self.df['custom_def'] == True)]
-
-        if filetype == 'none':
-            result = self.df[(self.df['ttb_check'] == 0) & (self.df['custom_def'] == False)]
-
-        result = result.drop('custom_def', axis=1)
-        self.df = result
-        return len(result)/len(cus_df)        
+        split_paths = {}
+        split_paths['topology'] = df[(df['ttb_check'] == 1) & (df['custom_def'] == False)].index
+        split_paths['custom'] = df[(df['ttb_check'] == 0) & (df['custom_def'] == True)].index
+        split_paths['both'] = df[(df['ttb_check'] == 1) & (df['custom_def'] == True)].index
+        split_paths['none'] = df[(df['ttb_check'] == 0) & (df['custom_def'] == False)].index
+        
+        return split_paths
     
 
-data = Data('tosca_and_general', 'all')
+data = Data('purpose')
