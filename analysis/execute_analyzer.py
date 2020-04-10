be3b0e2cfd4ffcd046ef2a8156d9d1363343d489
VaCH2 <chiel.van.asseldonk@hotmail.com>
2020-04-10 20:37:55 +0200

    blueprint for next steps

---
+++ analysis/execute_analyzer.py
@@ -0,0 +1,24 @@
+
+# Hier wordt de complete analzer uitgevoerd
+# Hier komt het stappenplan in zoals in het groot uitgeschreven
+
+# - general statistics for the entire dataset 
+# - Sparsity etc.
+
+# -define pipelines
+# - make data classes
+
+# pipelines = ['repo', 'professionality', 'purpose']
+
+# for elke pipeline:
+
+    # - descriptives
+    # - stats 
+    # - define verschillende subsets van metrics
+    # - siginificance analyis
+    # - clustering
+    # - evaluation?
+    # - onderin if main == __main__ etc.
+
+
+
