2d3400ca552390de22a36433fc3ee6e6d0c49f1a
VaCH2 <chiel.van.asseldonk@hotmail.com>
2020-05-04 09:12:42 +0200

    update

--- smells/detector/toomanyattributes.py
+++ smells/detector/toomanyattributes.py
@@ -2,35 +2,36 @@ import json
 import os
 from io import StringIO
 from smells.metrics.ngf import NGF
-
-#How to use these detectors? In here or will this just be a function
-#which returns true or false? Or better a number. 
+from toscametrics.yml.loc import LOC
 
 def evaluate_script_with_rule(filePath):
-    RULE_FILENAME = os.path.join(os.path.dirname(__file__), 'rule_stats.json')
+    # RULE_FILENAME = os.path.join(os.path.dirname(__file__), 'rule_stats.json')
 
-    with open(RULE_FILENAME) as f:
-        rule_dict = json.load(f)
+    # with open(RULE_FILENAME) as f:
+    #     rule_dict = json.load(f)
 
-    q1 = rule_dict['ngf']['q1']
-    q3 = rule_dict['ngf']['q3']
-    iqr = q3 - q1
+    # q1 = rule_dict['ngf']['q1']
+    # q3 = rule_dict['ngf']['q3']
+    # iqr = q3 - q1
 
     with open(filePath, 'r', encoding='utf8') as f:
         yml = f.read()
     yml = StringIO(yml.expandtabs(2))
-    metric = NGF(yml).count()
-
-    if (metric > q3 + 1.5*iqr) or (metric < q1 - 1.5*iqr):
-        smell_counter =1
-    else:
-        smell_counter = 0
+    attributes = NGF(yml).count()
+    loc_count = LOC(yml).count()
     
+    smell_detected = False
+    try:
+        if attributes / loc_count > 0.5:
+            smell_detected = True
+    except:
+        pass
+       
 
-    return smell_counter
+    return smell_detected
 
-path = r'C:\Users\s145559\OneDrive - TU Eindhoven\School\JADS\Jaar 2\Thesis\RADON PROJECT\Data\All\All\alien4cloud-config (3).yml'
-evaluate_script_with_rule(path)
+# path = r'C:\Users\s145559\OneDrive - TU Eindhoven\School\JADS\Jaar 2\Thesis\RADON PROJECT\Data\All\All\alien4cloud-config (3).yml'
+# evaluate_script_with_rule(path)
     
 
 
--- smells/metrics/ngf.py
+++ smells/metrics/ngf.py
@@ -1,5 +1,7 @@
 from io import StringIO
 from smells.metrics.blueprint_metric import BlueprintMetric
+from toscametrics.metrics.ninp import NINP
+from toscametrics.metrics.nout import NOUT
 
 class NGF(BlueprintMetric):
     """ This class is responsible for providing the methods to count the number of get functions (NGF) in a given .yaml file."""
@@ -7,15 +9,16 @@ class NGF(BlueprintMetric):
     def count(self):
         '''Returns the number of get_attribute, get_property, get_operation_output, and get_input instances '''
         
+        inputs = NINP(StringIO(self.getStringIOobject.expandtabs(2))).count()
+        outputs = NOUT(StringIO(self.getStringIOobject.expandtabs(2))).count()
+         
         raw_string = self.getStringIOobject
         get_attribute_count = raw_string.count('get_attribute')
         get_property_count = raw_string.count('get_property')
         get_operation_output_count = raw_string.count('get_operation_output')
         get_input_count = raw_string.count('get_input')
-        
 
-        return get_attribute_count + get_property_count + get_operation_output_count + get_input_count
-
+        return inputs + outputs + get_attribute_count + get_property_count + get_operation_output_count + get_input_count
 
 
 
