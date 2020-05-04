19dc20379661d741827ed66c26b0ba64e1604a86
VaCH2 <chiel.van.asseldonk@hotmail.com>
2020-05-04 08:00:12 +0200

    create significance analysis

--- analysis/significance.py
+++ analysis/significance.py
@@ -5,33 +5,41 @@ from scipy.stats import ks_2samp
 from statsmodels.stats.multitest import multipletests
 
 class Significance():
-    
-    def __init__(self, data1, data2):
+    def __init__(self, data1, data2, discardzeroes=False):
         self.data1 = data1
         self.data2 = data2
 
         if not list(self.data1.columns) == list(self.data2.columns):
             raise ValueError('datasets do not contain the same columns')
 
-        uncorrected_p_values = self.__calc_sig()
-        self.uncorrected_p_values = uncorrected_p_values.dropna() 
-        self.sig = self.__multitest_correction(self.uncorrected_p_values, 0.05)
-        self.rejected_features = self.sig[self.sig['rejected'] == True]
+        if len(self.data1) < 20 or len(self.data2) < 20:
+            #print('One of the samples contained less than 20 data points!')
+            self.rejected_features = pd.DataFrame()
+
+        else:
+            print('Both samples contained more than 20 data points! Total: ')
+            uncorrected_p_values = self.__calc_sig(discardzeroes)
+            self.uncorrected_p_values = uncorrected_p_values.dropna() 
+            self.sig = self.__multitest_correction(self.uncorrected_p_values, 0.01)
+            self.rejected_features = self.sig[self.sig['rejected'] == True]
 
     
-    def __calc_sig(self):
-        '''Calculates the p value for all the features based on the TO DETERMINE test
-        and returns a list of p-values. Each sample must contain at least 20 data points'''
-
-        if len(self.data1) < 20 or len(self.data2) < 20:
-            print('One of the samples contained less than 20 data points!')
+    def __calc_sig(self, discardzeroes):
+        '''Calculates the p value for all the features based on the U-test
+        and returns a list of p-values'''
         
         columns = self.data1.columns
         p_values = pd.DataFrame(columns=['p_values'])
 
         for column in columns:
             try:
-                stat, p = mannwhitneyu(self.data1[column], self.data2[column])
+                if discardzeroes:
+                    df1, df2 = self.__discard_zeroes_and_balance(self.data1, self.data2, column)
+                    if len(df1) < 20 or len(df2) < 20:
+                        continue                    
+                    stat, p = mannwhitneyu(df1[column], df2[column])
+                else:
+                    stat, p = mannwhitneyu(self.data1[column], self.data2[column])
 
                 value = pd.Series(data={'p_values' : p}, name=column)
                 p_values = p_values.append(value, ignore_index=False)
@@ -49,3 +57,21 @@ class Significance():
         d = {'rejected' : testcorrection[0], 'corr_p_values' : testcorrection[1]}
         p_values = pd.DataFrame(data=d, index=self.uncorrected_p_values.index)
         return p_values
+
+    def __discard_zeroes_and_balance(self, df1, df2, column):
+        df1 = df1[df1[column] != 0]
+        df2 = df2[df2[column] != 0]
+        # len_df1 = df1.shape[0]
+        # len_df2 = df2.shape[0]
+
+        # #downsampling
+        # if len_df1 < len_df2:
+        #     df2.sample(n=len_df1, replace=True, random_state=1)
+
+        # elif len_df2 < len_df1:
+        #     df1.sample(n=len_df2, replace=True, random_state=1)
+        
+        # else:
+        #     pass
+            
+        return df1, df2
---
+++ analysis/stat_signi_analysis.py
@@ -0,0 +1,124 @@
+from data import Data
+from stats import Stats
+from significance import Significance
+import itertools as it
+import plotly.express as px
+import plotly.graph_objects as go
+import pandas as pd
+import os
+
+root_folder = os.path.dirname(os.path.dirname( __file__ ))
+results_folder = os.path.join(root_folder, 'results', 'descriptive_report')
+
+def get_rejections(split, threshold, discardzeroes):
+    data = Data(split)
+    keys = list(data.dfs.keys())
+
+    scores = []
+    df_pvalues = pd.DataFrame(index=Data('all').dfs.get('all').columns)
+
+    for a,b in it.combinations(keys, 2):
+        df1 = data.dfs[a]
+        df2 = data.dfs[b]
+
+        if split == 'purpose':
+            df1 = df1.drop(['ttb_check', 'tdb_check', 'tob_check'], axis=1)
+            df2 = df2.drop(['ttb_check', 'tdb_check', 'tob_check'], axis=1)
+            df1 = df1.drop([key for key in df1.columns if 'cd' in key], axis=1)
+            df2 = df2.drop([key for key in df2.columns if 'cd' in key], axis=1)
+        else:
+            df1 = df1.drop(['ttb_check', 'tdb_check', 'tob_check'], axis=1)
+            df2 = df2.drop(['ttb_check', 'tdb_check', 'tob_check'], axis=1)
+
+        sig_analysis = Significance(df1, df2, discardzeroes)
+        try:
+            stat_values = sig_analysis.sig['corr_p_values']
+            df_pvalues = pd.concat([df_pvalues, stat_values], axis=1)
+        except:
+            continue
+
+        rejected_measurements = sig_analysis.rejected_features.index.values
+        scores.extend(rejected_measurements)
+
+    rejection_count = {}
+    for key in set(scores):
+        rejection_count[key] = scores.count(key)
+
+    df_rejection_count = pd.DataFrame.from_dict(rejection_count, orient='index', columns=['count'])
+    df_rejection_count = df_rejection_count[df_rejection_count['count'] > threshold] 
+    return df_rejection_count.sort_index(), df_pvalues.sort_index()
+
+
+
+
+def stats_per_split(split, sig_ix):
+    data = Data(split).dfs
+    for key, value in data.items():
+        print(key)
+        #value = value[value['nn_count'] != 0]
+        test = ['alien4cloud-csar-public-library', 'openstack-tosca-parser', 'radon-h2020-radon-particles', 'tliron-puccini', 'ystia-forge']
+        if key in test:
+            stats = Stats(value)
+            min = stats.min
+            max = stats.max
+            nonzero = stats.nonzero
+            zero = stats.zero
+            sparsity = stats.featuresparsity
+            mean = stats.mean
+            stddv = stats.stddv
+            q1 = stats.q1
+            median = stats.median
+            q3 = stats.q3
+            metric_properties = pd.concat([min, max, nonzero, zero, sparsity, mean, stddv, q1, median, q3], axis=1)
+            metric_properties  = metric_properties[metric_properties.index.isin(sig_ix)].sort_index()
+            metric_properties.to_excel(os.path.join(results_folder, f'{key}_stats.xlsx'))
+
+
+
+
+
+splits = ['professionality', 'purpose', 'repo']
+df_pvalues = pd.DataFrame(index=Data('all').dfs.get('all').columns)
+
+for split in splits:
+
+    rejection_count, pvalues = get_rejections(split, 0, False)
+    df_pvalues = pd.concat([df_pvalues, pvalues.mean(axis=1)], axis=1)
+
+    #Create stats per split
+    if split != 'repo':
+        stats_per_split(split, rejection_count.index.values)
+
+    #rejection_count = rejection_count.sort_values(by=['count'], ascending=False)
+    rejection_count.rename(index=lambda x: f'{x.split("_")[0].upper()} {x.split("_")[1]}', inplace=True)
+
+    fig = go.Figure(
+        go.Bar(
+            x=rejection_count.index.values,
+            y=rejection_count['count'],
+            marker=dict(color='rgb(0, 0, 100)')
+        )
+    )
+    fig.update_xaxes(
+        tickangle=45
+    )
+
+    fig.update_layout(
+        width=2551.2,
+        height=500,
+        paper_bgcolor='rgba(255, 255, 255, 1)', 
+        plot_bgcolor='rgba(255, 255, 255, 1)',
+        font=dict(size=21),
+        margin=dict(
+            l=0,
+            r=0,
+            b=0,
+            t=30
+        ),
+    )
+
+    fig.write_image(os.path.join(results_folder, f'{split}_rejectionbar.png'))
+
+df_pvalues.to_excel(os.path.join(results_folder, f'splits_rejectionpvalues.xlsx'))
+
+
