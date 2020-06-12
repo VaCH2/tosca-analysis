8934623d4fa078875480c6cdf76da5acbdf8f980
VaCH2 <chiel.van.asseldonk@hotmail.com>
2020-06-12 14:36:52 +0200

    refactor

--- analysis/case_study.py
+++ analysis/case_study.py
@@ -35,111 +35,110 @@ im = SmellEvaluator('im')
 
 # #--------------Internal and External measurement figure
 
-# def resetIndex(df):
-#     df['newIndex'] = [i for i in range(1, (df.shape[0] + 1))]
-#     df = df.set_index(keys='newIndex', drop=True)
-#     return df
+def resetIndex(df):
+    df['newIndex'] = [i for i in range(1, (df.shape[0] + 1))]
+    df = df.set_index(keys='newIndex', drop=True)
+    return df
 
-# dfsInternal = {
-#     'db' : resetIndex(db.evalDf).head(5),
-#     'tma' : resetIndex(tma.evalDf).head(5),
-#     'im' : resetIndex(im.evalDf).head(5) 
-# }
+dfsInternal = {
+    'db' : resetIndex(db.evalDf).head(5),
+    'tma' : resetIndex(tma.evalDf).head(5),
+    'im' : resetIndex(im.evalDf).head(5) 
+}
 
-# colors = {
-#     'db' : '#0057e7',
-#     'tma' : '#d62d20',
-#     'im' : '#ffa700'
-# }
+colors = {
+    'db' : '#0057e7',
+    'tma' : '#d62d20',
+    'im' : '#ffa700'
+}
 
-# names = {
-#     'db' : 'Duplicate Block',
-#     'tma' : 'Too many Attributes',
-#     'im' : 'Insufficient Modularization'
-# }
+names = {
+    'db' : 'Duplicate Block',
+    'tma' : 'Too many Attributes',
+    'im' : 'Insufficient Modularization'
+}
 
-# def createTrace(smell, pm, legend):
-#     df = dfsInternal[smell]
-#     trace = dict(
-#         type = 'scatter',
-#         x = df.index,
-#         y = df[pm],
-#         mode = 'lines',
-#         line = dict(color = colors[smell], width=6),
-#         name = names[smell],
-#         showlegend = legend
-#     )
-#     return trace
+def createTrace(smell, pm, legend):
+    df = dfsInternal[smell]
+    trace = dict(
+        type = 'scatter',
+        x = df.index,
+        y = df[pm],
+        mode = 'lines',
+        line = dict(color = colors[smell], width=6),
+        name = names[smell],
+        showlegend = legend
+    )
+    return trace
 
-# fig = make_subplots(rows=6, cols=1, shared_xaxes=True, subplot_titles=['Silhouette Score (Higher is better)', 'Calinski-Harabasz Index (Higher is better)', 'Davies-Bouldin  Index (Lower is better)', 'Precision (Higher is better)', 'Matthews Correlation Coefficient (Higher is better)', 'Adjusted Rand Index (Higher is better)'], vertical_spacing = 0.05)
+fig = make_subplots(rows=6, cols=1, shared_xaxes=True, subplot_titles=['Silhouette Score (Higher is better)', 'Calinski-Harabasz Index (Higher is better)', 'Davies-Bouldin  Index (Lower is better)', 'Precision (Higher is better)', 'Matthews Correlation Coefficient (Higher is better)', 'Adjusted Rand Index (Higher is better)'], vertical_spacing = 0.05)
 
-# legend = True
-# for ix, pm in enumerate(['sc', 'ch', 'db', 'precision', 'mcc', 'ari']):
-#     ix += 1
-#     fig.append_trace(createTrace('db', pm, legend), ix, 1)
-#     fig.append_trace(createTrace('tma', pm, legend), ix, 1)
-#     fig.append_trace(createTrace('im', pm, legend), ix, 1)
-#     legend = False
+legend = True
+for ix, pm in enumerate(['sc', 'ch', 'db', 'precision', 'mcc', 'ari']):
+    ix += 1
+    fig.append_trace(createTrace('db', pm, legend), ix, 1)
+    fig.append_trace(createTrace('tma', pm, legend), ix, 1)
+    fig.append_trace(createTrace('im', pm, legend), ix, 1)
+    legend = False
 
-# fig.update_layout(
-#     height=2900, 
-#     width=2800, 
-#     title_text="Top 5 Configurations",
-#     paper_bgcolor='rgba(255, 255, 255, 1)',
-#     plot_bgcolor='rgba(255, 255, 255, 1)',
-#     legend=dict(x=-.1, y=1.2, orientation='h'),
-#     font = dict(size=47)
-#     )
+fig.update_layout(
+    height=2900, 
+    width=2800, 
+    title_text="Top 5 Configurations",
+    paper_bgcolor='rgba(255, 255, 255, 1)',
+    plot_bgcolor='rgba(255, 255, 255, 1)',
+    legend=dict(x=-.1, y=1.2, orientation='h'),
+    font = dict(size=47)
+    )
 
-# #fix subplot title size
-# for i in fig['layout']['annotations']:
-#     i['font'] = dict(size=47)
+#fix subplot title size
+for i in fig['layout']['annotations']:
+    i['font'] = dict(size=47)
 
-# fig.show()
-# fig.write_image(os.path.join(results_folder, 'configurationperformance.png'))
+fig.show()
+fig.write_image(os.path.join(results_folder, 'configurationperformance.png'))
 
 
-# #-----------Stability
-# (sparse, corr, out, pca, dista, algo)
-# dbTopConfigs = [
-#     (False, False, False, True, None, ('gm', 'spherical')),
-#     (False, False, True, True, None, ('gm', 'spherical')),
-#     (False, False, True, True, 'braycurtis', ('kmedoids', None)),
-#     (False, False, False, True, None, ('gm', 'tied')),
-#     (False, True, True, True, None, ('gm', 'full')),
-# ]
+#-----------Stability
+dbTopConfigs = [
+    (False, False, False, True, None, ('gm', 'spherical')),
+    (False, False, True, True, None, ('gm', 'spherical')),
+    (False, False, True, True, 'braycurtis', ('kmedoids', None)),
+    (False, False, False, True, None, ('gm', 'tied')),
+    (False, True, True, True, None, ('gm', 'full')),
+]
  
-# tmaTopConfigs = [
-#     (False, False, False, True, None, ('gm', 'full')),
-#     (False, False, False, True, None, ('gm', 'tied')),
-#     (False, False, False, True, None, ('gm', 'spherical')),
-#     (False, False, True, True, None, ('gm', 'spherical')),
-#     (False, False, False, True, 'l1', ('agglo', 'complete'))
-# ]
+tmaTopConfigs = [
+    (False, False, False, True, None, ('gm', 'full')),
+    (False, False, False, True, None, ('gm', 'tied')),
+    (False, False, False, True, None, ('gm', 'spherical')),
+    (False, False, True, True, None, ('gm', 'spherical')),
+    (False, False, False, True, 'l1', ('agglo', 'complete'))
+]
 
 
-# imTopConfigs = [
-#     (False, False, False, True, None, ('gm', 'full')),
-#     (False, False, False, True, None, ('gm', 'tied')),
-#     (False, False, True, True, None, ('gm', 'full')),
-#     (False, False, True, True, None, ('gm', 'spherical')),
-#     (False, False, False, True, None, ('gm', 'spherical'))
-# ]
+imTopConfigs = [
+    (False, False, False, True, None, ('gm', 'full')),
+    (False, False, False, True, None, ('gm', 'tied')),
+    (False, False, True, True, None, ('gm', 'full')),
+    (False, False, True, True, None, ('gm', 'spherical')),
+    (False, False, False, True, None, ('gm', 'spherical'))
+]
 
-# for ix, dbTopConfig in enumerate(dbTopConfigs):
-#     dbTopConfigModel = ClusterConfigurator(db.df, dbTopConfig)
-#     stability = dbTopConfigModel.getStability()
-#     print(f'DB config {ix}: {stability[0]}')
+for ix, dbTopConfig in enumerate(dbTopConfigs):
+    dbTopConfigModel = ClusterConfigurator(db.df, dbTopConfig)
+    stability = dbTopConfigModel.getStability()
+    print(f'DB config {ix}: {stability[0]}')
 
-# for ix, tmaTopConfig in enumerate(tmaTopConfigs):
-#     tmaTopConfigModel = ClusterConfigurator(tma.df, tmaTopConfig)
-#     stability = tmaTopConfigModel.getStability()
-#     print(f'TmA config {ix}: {stability[0]}')
+for ix, tmaTopConfig in enumerate(tmaTopConfigs):
+    tmaTopConfigModel = ClusterConfigurator(tma.df, tmaTopConfig)
+    stability = tmaTopConfigModel.getStability()
+    print(f'TmA config {ix}: {stability[0]}')
 
-# for ix, imTopConfig in enumerate(imTopConfigs):
-#     imTopConfigModel = ClusterConfigurator(im.df, imTopConfig)
-#     stability = imTopConfigModel.getStability()
-#     print(f'IM config {ix}: {stability[0]}')
+for ix, imTopConfig in enumerate(imTopConfigs):
+    imTopConfigModel = ClusterConfigurator(im.df, imTopConfig)
+    stability = imTopConfigModel.getStability()
+    print(f'IM config {ix}: {stability[0]}')
 
 
 #----------------------Comparision
@@ -184,8 +183,9 @@ def main():
             scoreDict[f'cluster-{smell}-mcc'].append(calculateScore('mcc', sampleLabels[smell], clusterLabels))
             scoreDict[f'cluster-{smell}-precision'].append(calculateScore('precision', sampleLabels[smell], clusterLabels))
     
-    pickle.dump(scoreDict, open(os.path.join(temp_folder, f'MCCandPrecisionFor{iters}iters{subSample}percent'), 'wb'))
+    #pickle.dump(scoreDict, open(os.path.join(temp_folder, f'MCCandPrecisionFor{iters}iters{subSample}percent'), 'wb'))
     #scoreDict = pickle.load(open(os.path.join(temp_folder, f'MCCandPrecisionFor{iters}iters{subSample}percent'), 'rb'))
+
     statisticalTest(scoreDict)
     boxplotCreation(scoreDict)
     return sampleLabels, clusterLabels, ruleLabels
@@ -313,15 +313,13 @@ def boxplotCreation(scoreDict):
         return box
     
     #MCC
-    fig = make_subplots(rows=1, cols=6, shared_yaxes=True, subplot_titles=['DB', 'DB', 'TmA', 'TmA', 'IM', 'IM'])
+    fig = make_subplots(rows=1, cols=6, shared_yaxes=True, subplot_titles=['Duplicate Block', '', 'Too many Attributes', '', 'Insufficient Modularization', ''])
     for ix, combi in enumerate(['rule-db-mcc', 'cluster-db-mcc', 'rule-tma-mcc', 'cluster-tma-mcc', 'rule-im-mcc', 'cluster-im-mcc']):
         ix += 1
         fig.append_trace(createTrace(combi, scoreDict), 1, ix)
     fig.update_layout(
         height=1300, 
         width=2800, 
-        #margin=dict(l=0, r=0, t=200, b=0),
-        #title_text="Matthew's Correlation Coefficient (MCC)",
         paper_bgcolor='rgba(255, 255, 255, 1)',
         plot_bgcolor='rgba(255, 255, 255, 1)',
         showlegend=False,
@@ -335,19 +333,18 @@ def boxplotCreation(scoreDict):
 
 
     #Precision
-    fig = make_subplots(rows=1, cols=6, shared_yaxes=True, subplot_titles=['DB', 'DB', 'TmA', 'TmA', 'IM', 'IM'])
+    fig = make_subplots(rows=1, cols=6, shared_yaxes=True, subplot_titles=['Duplicate Block', '', 'Too many Attributes', '', 'Insufficient Modularization', ''])
     for ix, combi in enumerate(['rule-db-precision', 'cluster-db-precision', 'rule-tma-precision', 'cluster-tma-precision', 'rule-im-precision', 'cluster-im-precision']):
         ix += 1
         fig.append_trace(createTrace(combi, scoreDict), 1, ix)
     fig.update_layout(
         height=1300, 
         width=2800, 
-        #margin=dict(l=0, r=0, t=200, b=0),
-        #title_text="Matthew's Correlation Coefficient (precision)",
         paper_bgcolor='rgba(255, 255, 255, 1)',
         plot_bgcolor='rgba(255, 255, 255, 1)',
         showlegend=False,
-        font = dict(size=47)
+        font = dict(size=47),
+        yaxis=dict(range=[0,0.7])
         )
     #fix subplot title size
     for i in fig['layout']['annotations']:
@@ -356,156 +353,3 @@ def boxplotCreation(scoreDict):
     fig.write_image(os.path.join(results_folder, f'comparison50sampleprecision{iters}iters{subSample}percent.png'))
 
 x, y ,z  = main()
-
-
-
-
-
-#HIER DADELIJK NOG DE TOP ANALYSEREN EN OOK DE VERGELIJKING MET SCHWARZ MAKEN
-
-
-# #Op deze manier kan je dan door een 
-# topModel = SmellEvaluator('db').getPickle('db', test.evalDf.iloc[4].name)
-# #Or if top one:
-# topModel = SmellEvaluator('db').getPickle('db', test.topconfig.name)
-# topModel.getStability()
-
-#Als we dan de stability willen bereken moeten we m ff opnieuw aanroepen(nu tenminste, kan evt wel in loop)
-#top_db = ClusterConfigurator()
-
-
-#----------------old
-#Only top 1
-#df = df.head(10)
-
-# for ix in range(0, 1):
-#     print(df.iloc[ix]['name'], df.iloc[ix]['#clusterTrue'], df.iloc[ix]['#clusterFalse'])
-
-# #Then, we calculate statistically test the clusters with eachother to find out which features are statistically different. 
-# rejected_features = pd.Series()
-# for ix in df['name']:
-#     print(f'############## {ix} #############')
-#     x_df = pickle.load(open(os.path.join(root_folder, 'temp_data', 'clustering', f'clusteringpipeline_smell-{smell}_{ix}'), 'rb')) 
-#     dfTrue = x_df[x_df['cluster'] == True]
-#     dfFalse = x_df[x_df['cluster'] == False]
-#     dfTrue.drop(['cluster', smell], axis=1,  inplace=True)
-#     dfFalse.drop(['cluster', smell], axis=1,  inplace=True)
-
-#     sig_measurements = []
-#     for measurement in dfTrue.columns:
-#         print(f'------------------------{measurement}--------------------------')
-#         try:
-#             stat, pvalue = mannwhitneyu(dfTrue[measurement], dfFalse[measurement])
-#             #print('P-value: ', pvalue)
-#             if pvalue < 0.05:
-#                 sig_measurements.append((measurement, pvalue))
-#         except:
-#             pass
-
-#     print('# of Measurements: ', len(dfTrue.columns))
-#     print('# of rejected Measurements: ', len(sig_measurements))
-
-        # if pvalue < 0.05:
-        #     fig = px.box(x_df, x='cluster', y=measurement, points='all')
-        #     fig.write_image(os.path.join(results_folder, smell, f'{ix}_{measurement}.png'))
-            #fig.show()
-
-
-
-
-        #     print('True (smelly) distribution:')
-        #     serie = dfTrue[measurement]
-        #     zero_serie = serie[serie == 0]
-        #     real_serie = serie[serie != 0]
-
-        #     #MAKE FIGURE
-        #     custom_font=dict(size=15)
-        #     fig = make_subplots(
-        #         rows=1,
-        #         cols=2,
-        #         specs=[[{}, {}]], 
-        #         column_widths=[0.2, 0.8])
-
-
-        #     fig.add_trace(
-        #         go.Bar(
-        #             x=list(zero_serie.value_counts().index),
-        #             y=zero_serie.value_counts().tolist(),
-        #             marker=dict(color='rgb(210,89,89)')
-        #         ),
-        #         row=1,
-        #         col=1
-        #     )
-
-        #     fig.add_trace(
-        #         go.Histogram(
-        #             x=real_serie,
-        #             xbins=dict(
-        #                 start=np.min(real_serie),
-        #                 end=np.max(real_serie),
-        #                 size=(np.max(real_serie) - np.min(real_serie))/20
-        #                 ),
-        #                 marker=dict(color='rgb(0, 0, 100)')
-        #         ),
-        #         row=1,
-        #         col=2
-        #     )
-
-        #     fig.update_layout(
-        #         showlegend=False,
-        #         bargap=0.2,
-        #         paper_bgcolor='rgba(255, 255, 255, 1)', 
-        #         plot_bgcolor='rgba(255, 255, 255, 1)'
-        #     )
-
-        #     fig.show()
-
-
-        #     print('False (Non smelly) distribution:')
-        #     serie = dfFalse[measurement]
-        #     zero_serie = serie[serie == 0]
-        #     real_serie = serie[serie != 0]
-
-        #     #MAKE FIGURE
-        #     custom_font=dict(size=15)
-        #     fig = make_subplots(
-        #         rows=1,
-        #         cols=2,
-        #         specs=[[{}, {}]], 
-        #         column_widths=[0.2, 0.8])
-
-
-        #     fig.add_trace(
-        #         go.Bar(
-        #             x=list(zero_serie.value_counts().index),
-        #             y=zero_serie.value_counts().tolist(),
-        #             marker=dict(color='rgb(210,89,89)')
-        #         ),
-        #         row=1,
-        #         col=1
-        #     )
-
-        #     fig.add_trace(
-        #         go.Histogram(
-        #             x=real_serie,
-        #             xbins=dict(
-        #                 start=np.min(real_serie),
-        #                 end=np.max(real_serie),
-        #                 size=(np.max(real_serie) - np.min(real_serie))/20
-        #                 ),
-        #                 marker=dict(color='rgb(0, 0, 100)')
-        #         ),
-        #         row=1,
-        #         col=2
-        #     )
-
-        #     fig.update_layout(
-        #         showlegend=False,
-        #         bargap=0.2,
-        #         paper_bgcolor='rgba(255, 255, 255, 1)', 
-        #         plot_bgcolor='rgba(255, 255, 255, 1)'
-        #     )
-
-        #     fig.show()
-
-
--- analysis/utils.py
+++ analysis/utils.py
@@ -3,6 +3,7 @@ import copy
 import numpy as np
 
 
+
 def scale_df(df):
     copy_df = df.copy(deep=True)
     scaler = StandardScaler()
@@ -40,15 +41,3 @@ def df_minus_df(df1, df2):
     return df
 
 
-# function to calculate Cohen's d for independent samples
-def cohend(d1, d2):
-	# calculate the size of samples
-	n1, n2 = len(d1), len(d2)
-	# calculate the variance of the samples
-	s1, s2 = np.var(d1, ddof=1), np.var(d2, ddof=1)
-	# calculate the pooled standard deviation
-	s = np.sqrt(((n1 - 1) * s1 + (n2 - 1) * s2) / (n1 + n2 - 2))
-	# calculate the means of the samples
-	u1, u2 = np.mean(d1), np.mean(d2)
-	# calculate the effect size
-	return (u1 - u2) / s
